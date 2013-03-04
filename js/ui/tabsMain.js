fe32a5ea38bd02ff38788a3a226600f47e9c08dc
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2013-03-04 01:47:02 +0100

    Event arguments as attributes instead of array

--- js/ui/tabsMain.js
+++ js/ui/tabsMain.js
@@ -59,10 +59,10 @@ _priv.TabsMain = function(tabsId, shareit, preferencesDialogOpen)
   shareit.addEventListener('transfer.begin', tabDownloading_checkAndUpdate);
   shareit.addEventListener('transfer.update', function(event)
   {
-    var type = event.type;
+    var fileentry = event.fileentry;
     var value = event.value;
 
-    $(tabDownloading).trigger(type, [value]);
+    $(tabDownloading).trigger(fileentry, [value]);
   });
   shareit.addEventListener('transfer.end', tabDownloading_checkAndUpdate);
 
