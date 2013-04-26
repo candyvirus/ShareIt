1c769a2e60ee13c36b72bd8f4f39712af88df2f9
Jesús Leganés Combarro "Piranna" <piranna@gmail.com>
2013-04-26 22:37:16 +0200

    Created classes for Folder and Entry sharedpoints

--- js/ui/dialogConfig.js
+++ js/ui/dialogConfig.js
@@ -87,7 +87,7 @@ _priv.DialogConfig = function(dialogId, options, shareit)
 
     policy(function()
     {
-      shareit.sharedpointsManager_addSharedpoint('folder', files, sharedpoint_added);
+      shareit.sharedpointsManager_add('folder', files, sharedpoint_added);
 
       // Reset the input after send the files to hash
       input.val('');
@@ -113,7 +113,7 @@ _priv.DialogConfig = function(dialogId, options, shareit)
       if(entry.isDirectory)
         policy(function()
         {
-          shareit.sharedpointsManager_addSharedpoint('entry', entry, sharedpoint_added);
+          shareit.sharedpointsManager_add('entry', entry, sharedpoint_added);
         });
 
       else
